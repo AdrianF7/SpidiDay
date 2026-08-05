20931418bdac5348465e21c00443799437b0e8cb
Adrian Camilo Fontalvo Peña <adriancamilofanta@gmail.com>
2026-08-04 23:19:44 -0500

    Corregir encuadre del icono de SpidiDay

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE='spididay-v2.3.0';
-const APP_SHELL=['./','./index.html','./styles.css?v=19','./data/quotes.js?v=19','./data/motivation-messages.js?v=1','./db.js?v=12','./app.js?v=1.1.0','./story.js?v=2.1.0','./compass.js?v=15','./data/verses-rv1909.js','./manifest.webmanifest?v=2','./mascot-spidi-marea.svg?v=12','./decorations-marea.svg','./assets/origin-spididay.png','./fonts/Manrope-Variable.ttf','./fonts/Fraunces-Variable.ttf','./icons/icon-192.png?v=2','./icons/icon-512.png?v=2'];
+﻿const CACHE='spididay-v2.3.1';
+const APP_SHELL=['./','./index.html','./styles.css?v=19','./data/quotes.js?v=19','./data/motivation-messages.js?v=1','./db.js?v=12','./app.js?v=1.1.0','./story.js?v=2.1.0','./compass.js?v=15','./data/verses-rv1909.js','./manifest.webmanifest?v=2','./mascot-spidi-marea.svg?v=13','./decorations-marea.svg','./assets/origin-spididay.png','./fonts/Manrope-Variable.ttf','./fonts/Fraunces-Variable.ttf','./icons/icon-192.png?v=2','./icons/icon-512.png?v=2'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
@@ -10,6 +10,7 @@ self.addEventListener('fetch',event=>{
   }
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response})));
 });
+
 
 
 
